9948ad97a870e30bd5c030d71f7f6f90accbb448
WilliKoller <64897730+WilliKoller@users.noreply.github.com>
2025-04-01 15:42:30 +0200

    update exported version and batch mode

--- batchProcessParticipant.m
+++ batchProcessParticipant.m
@@ -1,21 +1,29 @@
 app = processC3D_exported;
-app.path = 'C:\Users\Willi\SynologyDrive\Hanging_Paper\Juliana\09_05_2023';
-app.emgLabelCSVPath = 'C:\Users\Willi\SynologyDrive\Hanging_Paper\emg_labels_d2_v2.csv';
-fileList = dir([app.path '\*.c3d']);
+app.path = 'pathToC3DFiles';
+% app.emgLabelCSVPath = 'C:\Users\Willi\SynologyDrive\Hanging_Paper\emg_labels_d2_v2.csv';
+fileList = dir(fullfile(app.path, '**', '*.c3d'));
 
-for i = 1: numel(fileList)
+for i = 1 : numel(fileList)
+    app.path = fileList(i).folder;
     app.c3dFileName = fileList(i).name;
-    disp(['processing' app.c3dFileName]);
-    
-    app.ClimbingCheckBox.Value = 1;
-    app.detectwalkingdirectionautomaticallybymarkerCheckBox.Value = 0;
-    app.detectforceplatesautomaticallyCheckBox.Value = 0;
+    if ~strcmp(app.c3dFileName, 'c3dfile.c3d')
+        disp(['processing ' app.path(end-20 : end) ' - ' app.c3dFileName]);
 
-    app.filtermarkersCheckBox.Value = 0;
-    app.filterGRFsCheckBox.Value = 0;
-    app.setGRFstozerooutsideregionoffootcontactCheckBox.Value = 0;
+        % app.ClimbingCheckBox.Value = 1;
+        app.detectwalkingdirectionautomaticallybymarkerCheckBox.Value = 0;
+        app.rotatearoundyaxisDropDown.Value = '270°';
+        app.detectforceplatesautomaticallyCheckBox.Value = 1;
+        app.Ignorec3deventsCheckBox.Value = 0;
+        app.doPostZeroLevellingCheckBox.Value = 1;
+        app.validfootstrikeeventsLabel.Visible = "on";
+        app.forceplatecontactsLabel.Visible = "off";
 
-    app.processC3Dfile();
+        app.filtermarkersCheckBox.Value = 0;
+        app.filterEMGandexporttostoCheckBox.Value = 0;
+        app.filterGRFsCheckBox.Value = 1;
 
-    app.CreatefilesButtonPushed(0);
+        app.processC3Dfile();
+
+        app.CreatefilesButtonPushed(0);
+    end
 end
